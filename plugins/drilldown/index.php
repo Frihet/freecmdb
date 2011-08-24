434c88f2e82bf7ff47d567f57dc8a0678d233e08
Axel Lingoncvantz <axel.liljencrantz@freecode.no>
2011-08-24 09:19:01 +0200

    Added scrollbar to ci selector popup window

--- plugins/drilldown/index.php
+++ plugins/drilldown/index.php
@@ -5,7 +5,7 @@ class DrilldownCiSelector
     function make($name, $selected)
     {
         return '<input type="hidden" name="' . htmlEncode($name) . '" id="' . htmlEncode($name) . '" value="' . htmlEncode($selected) . '" />
-    <div id="ci_picker_code"></div>
+    <div id="ci_picker_code" style="max-height: 400px; overflow: auto"></div>
     <script>
       $("#ci_picker_code").load("/FreeCMDB/plugins/drilldown/drilldown?update_target='.htmlEncode($name).'&embed_point=ci_picker_root&time='.time().'");
     </script>
